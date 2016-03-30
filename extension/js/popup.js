a5f9ae5fbc549e12ab71839f0a48808ffb0cf9d0
wkmghn <tailshade@hotmail.com>
2016-03-30 23:50:06 +0900

    途中経過をサブミット

---
+++ extension/js/popup.js
@@ -0,0 +1,6 @@
+﻿//window.onload = function () {
+    //var el = document.getElementById("hello");
+    //el.addEventListener("click", function () {
+        alert("外部JavaScriptファイル");
+    //}, false);
+//};
